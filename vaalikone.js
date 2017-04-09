1308a7c8763c18325bf0c08fd548e1d493601247
Otto Seiskari <otto.seiskari@indooratlas.com>
2017-04-09 14:32:17 +0300

    opinion mode

--- vaalikone.js
+++ vaalikone.js
@@ -1,13 +1,30 @@
 "use strict";
 /* globals _, d3 */
 
-function Main(questions, peopleWithAnswers, d3root) {
-    this.cities = _.uniq(peopleWithAnswers.map(p => p.person.city));
+
+function preprocessAnswers(peopleWithAnswers) {
 
     peopleWithAnswers.forEach(person => {
         // round all answers to integer values
         person.answers = _.mapValues(person.answers, Math.round);
     });
+
+    const answerOptions = _.uniq(
+        _.flatten(peopleWithAnswers.map(d => _.values(d.answers)))
+    );
+
+    const neutralAnswer = _.mean(answerOptions);
+
+    peopleWithAnswers.forEach(person => {
+        // round all answers to integer values
+        person.answers = _.mapValues(person.answers, a => a - neutralAnswer);
+    });
+}
+
+function Main(questions, peopleWithAnswers, d3root) {
+    this.cities = _.uniq(peopleWithAnswers.map(p => p.person.city));
+
+    preprocessAnswers(peopleWithAnswers);
 
     function getPeopleForCity(city) {
         if (city === undefined) {
@@ -28,13 +45,12 @@ function Main(questions, peopleWithAnswers, d3root) {
     this.render(d3root);
 }
 
-
 Main.prototype.renderCity = function(city) {
     new Vaalikone(
             this.getQuestionsForCity(city),
             this.getPeopleForCity(city))
-        .render(this.questionList, this.graphColumn);
-}
+        .start(this.questionList, this.graphColumn);
+};
 
 Main.prototype.render = function(d3root) {
     const container = d3root
@@ -73,10 +89,9 @@ Main.prototype.render = function(d3root) {
             .classed('graph-column', true);
 
     this.renderCity();
-}
+};
 
 function Vaalikone(questions, peopleWithAnswers) {
-
 
     const partyAnswers = d3.nest()
         .key(d => d.person.party)
@@ -84,7 +99,7 @@ function Vaalikone(questions, peopleWithAnswers) {
 
     this.questions = questions;
     this.parties = partyAnswers.map(d => d.key);
-    this.cities = _.uniq(peopleWithAnswers.map(p => p.person.city));
+    this.peopleWithAnswers = peopleWithAnswers;
 
     this.partyAnswerMatrix = _.fromPairs(
         partyAnswers.map(group => [
@@ -105,16 +120,34 @@ function Vaalikone(questions, peopleWithAnswers) {
     );
     this.answerOptions.sort();
 
+    this.opinions = {};
+
     this.subplotSize = {
         w: 200,
         h: 50
     };
+
     this.defineColors();
 
     //console.log(this.answerOptions);
     //console.log(this.partyAnswerMatrix);
-    //console.log(this.cities);
 }
+
+Vaalikone.prototype.toggleOpinion = function(question, newOpinion) {
+    if (this.opinions[question] === newOpinion) {
+        delete this.opinions[question];
+    } else {
+        this.opinions[question] = newOpinion;
+    }
+    if (_.isEmpty(this.opinions)) {
+        this.render();
+    } else {
+        //console.log(this.opinions);
+        this.renderQuestionList();
+        this.graphColumn.html('');
+        this.renderOpinionMatches();
+    }
+};
 
 Vaalikone.prototype.defineColors = function() {
 
@@ -147,15 +180,15 @@ Vaalikone.prototype.defineColors = function() {
 };
 
 Vaalikone.prototype.computeMean = function(histogram) {
-    return _.sum(_.map(histogram, (k, v) => k*v)) / _.sum(_.values(histogram));
+    return _.sum(_.map(histogram, (v, k) => k*v)) / _.sum(_.values(histogram));
 };
 
-Vaalikone.prototype.renderHistogram = function(d3root, data) {
+Vaalikone.prototype.renderHistogram = function(d3root, data, bins) {
 
     const total = _.sum(_.values(data));
 
-    const barWidth = this.subplotSize.w / (this.answerOptions.length+1);
-    const minX = d3.min(this.answerOptions);
+    const barWidth = this.subplotSize.w / (bins.length+1);
+    const minX = d3.min(bins);
     const maxR = barWidth / Math.sqrt(2);
     const maxHeight = this.subplotSize.h;
     const xStart = 0.5 / Math.sqrt(2) * barWidth;
@@ -164,7 +197,7 @@ Vaalikone.prototype.renderHistogram = function(d3root, data) {
 
     const bars = d3root
         .selectAll('circle')
-        .data(this.answerOptions);
+        .data(bins);
 
     bars.exit().remove();
 
@@ -181,7 +214,7 @@ Vaalikone.prototype.renderHistogram = function(d3root, data) {
     return this.textColormap(this.computeMean(data));
 };
 
-Vaalikone.prototype.renderPartyRow = function(d3root, party, data, questionId, selectedParty) {
+Vaalikone.prototype.renderPartyRow = function(d3root, party, data, bins, onClick, selectedParty) {
 
     const title = d3root
         .selectAll('div.title')
@@ -195,10 +228,7 @@ Vaalikone.prototype.renderPartyRow = function(d3root, party, data, questionId, s
             .classed('col-4', true)
         .merge(title)
             .text(d => d)
-            .on('click', d => {
-                that.renderQuestion(questionId, party);
-                that.renderQuestionList(party);
-            });
+            .on('click', onClick);
 
     const histogram = d3root
         .selectAll('div.histogram')
@@ -213,7 +243,7 @@ Vaalikone.prototype.renderPartyRow = function(d3root, party, data, questionId, s
                     .append('svg')
                         .attr('width', this.subplotSize.w)
                         .attr('height', this.subplotSize.h)
-                    .merge(histogram), data);
+                    .merge(histogram), data, bins);
 
     if (selectedParty) {
         titleText.style('font-weight', selectedParty === party ? 'bold' : 'normal');
@@ -225,7 +255,6 @@ Vaalikone.prototype.renderPartyRow = function(d3root, party, data, questionId, s
 
 Vaalikone.prototype.renderQuestion = function(questionId, selectedParty) {
 
-    const options = this.answerOptions;
     const byParty = _.mapValues(this.partyAnswerMatrix,
         answers => _.fromPairs(d3.nest()
             .key(d => d).sortKeys(d3.ascending)
@@ -238,11 +267,11 @@ Vaalikone.prototype.renderQuestion = function(questionId, selectedParty) {
     const that = this;
     const parties = _.keys(byParty).filter(p => !_.isEmpty(byParty[p]));
     const means = _.fromPairs(
-        parties.map(p => [p, -that.computeMean(byParty[p])]));
+        parties.map(p => [p, that.computeMean(byParty[p])]));
 
     const subplots = this.graphColumn
         .selectAll('div.row')
-        .data(_.sortBy(parties, p => means[p]));
+        .data(_.sortBy(parties, p => -means[p]));
 
     subplots.exit().remove();
 
@@ -251,8 +280,86 @@ Vaalikone.prototype.renderQuestion = function(questionId, selectedParty) {
             .classed('row', true)
         .merge(subplots)
             .transition()
-            .each(function(d) {
-                that.renderPartyRow(d3.select(this), d, byParty[d], questionId, selectedParty);
+            .each(function(party) {
+                function onClick() {
+                    that.renderQuestion(questionId, party);
+                    that.renderQuestionList(party);
+                }
+                that.renderPartyRow(d3.select(this),
+                    party,
+                    byParty[party],
+                    that.answerOptions,
+                    onClick,
+                    selectedParty);
+            });
+};
+
+Vaalikone.prototype.renderOpinionMatches = function() {
+
+    const selected = _.keys(this.opinions);
+    const that = this;
+
+    function answerMatch(opinion, ans) {
+        return opinion * ans;
+    }
+
+    const personMatches = this.peopleWithAnswers.map(p => {
+        const answered = _.pickBy(
+            _.pick(p.answers, selected),
+            (v, k) => v !== undefined);
+
+        let match = 0;
+        if (!_.isEmpty(answered)) {
+            match = _.sum(_.map(answered, (ans, qId) =>
+                answerMatch(that.opinions[qId], ans)));
+        }
+
+        return { 'person': p.person, 'match': match };
+    });
+
+    const allValues = _.uniq(_.flatMap(personMatches, p => p.match));
+    const bins = _.range(d3.min(allValues), d3.max(allValues));
+
+    //console.log(bins);
+
+    const partyPeople = _.fromPairs(d3.nest()
+        .key(p => p.person.party)
+        .entries(personMatches)
+        .map(obj => [obj.key, obj.values]));
+
+    //console.log(partyPeople);
+
+    const byParty =  _.mapValues(partyPeople,
+        people => _.fromPairs(d3.nest()
+            .key(d => d).sortKeys(d3.ascending)
+            .rollup(d => d.length)
+            .entries(people.map(p => p.match))
+            .map(obj => [obj.key, obj.value])));
+
+    //console.log(byParty);
+
+    const means = _.mapValues(byParty, that.computeMean);
+
+    //console.log(means);
+
+    const subplots = this.graphColumn
+        .selectAll('div.row')
+        .data(_.sortBy(_.keys(byParty), p => -means[p]));
+
+    subplots.exit().remove();
+
+    subplots.enter()
+            .append('div')
+            .classed('row', true)
+        .merge(subplots)
+            .transition()
+            .each(function(party) {
+                function onClick() {}
+                that.renderPartyRow(d3.select(this),
+                    party,
+                    byParty[party],
+                    bins,
+                    onClick);
             });
 };
 
@@ -290,21 +397,41 @@ Vaalikone.prototype.renderQuestionList = function(party) {
 
     this.questionList.html(''); // clear
 
-    const questions = this.questionList
-        .selectAll('li a')
-        .data(data);
-
     const that = this;
 
     const NO_ACTION = 'javascript:void(0)';
 
-    const questionTexts = questions.enter()
-            .append('li')
+
+    const questions =  this.questionList
+        .selectAll('li')
+        .data(data)
+        .enter()
+            .append('li');
+
+    function addArrow(value, symbol, color) {
+        questions
             .append('a')
-                .attr('href', NO_ACTION)
-        .merge(questions)
+            .attr('href', NO_ACTION)
+            .classed('strong', true)
+            .html(symbol)
+            .style('color', d => {
+                if (that.opinions[d.id] === value) {
+                    return color;
+                } else {
+                    return 'black';
+                }
+            })
+            .on('mousedown', d => that.toggleOpinion(d.id, value));
+    }
+
+    addArrow(+1, '&#8679;', 'green');
+    addArrow(-1, '&#8681;', 'red');
+
+    questions
+            .append('a')
+            .attr('href', NO_ACTION)
             .on('mousedown', d => that.renderQuestion(d.id, party))
-            .text(d => d.text)
+            .text(d => ' ' + d.text)
             .attr('style', d => {
                 if (party) {
                     return 'color: ' + that.textColormap(d.partyValue);
@@ -313,10 +440,13 @@ Vaalikone.prototype.renderQuestionList = function(party) {
             });
 };
 
-Vaalikone.prototype.render = function(questionList, graphColumn) {
-    this.questionList = questionList;
-    this.graphColumn = graphColumn;
-
+Vaalikone.prototype.render = function() {
     this.renderQuestionList();
     this.graphColumn.html('');
+}
+
+Vaalikone.prototype.start = function(questionList, graphColumn) {
+    this.questionList = questionList;
+    this.graphColumn = graphColumn;
+    this.render();
 };
