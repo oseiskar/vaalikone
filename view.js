629ca2294ac433974804ec40f31afa87af69214a
Otto Seiskari <otto.seiskari@gmail.com>
2019-03-23 15:45:11 +0200

    first version of two different views

--- view.js
+++ view.js
@@ -25,6 +25,18 @@ function createApp(vueElement) {
       .map(v => (v*sat + 1.0 - sat)*bright));
   }
 
+
+  function _textColorMap(x, options) {
+    if (x === undefined || x === null) return null;
+    return 'color: ' + colorMap(x, options.minScore, options.maxScore, 0.7);
+  }
+
+  function _getHTMLArrow(score) {
+    if (score > 0) return '&#8679;'; // arrow up
+    else if (score < 0) return '&#8681;'; // arrow down
+    return '&#9898;'; // hollow circle
+  }
+
   Vue.component('histogram-plot', {
     template: `
       <svg :width="dimensions.width" :height="dimensions.height">
@@ -54,45 +66,17 @@ function createApp(vueElement) {
     }
   });
 
-  const app = new Vue({
-    el: vueElement,
-    data: {
-      copyright: {},
-      questions: [],
-      allParties: [],
-      options: [],
-      cities: [],
-      error: null,
-      selected: {
-        party: null,
-        city: null
-      },
-      opinions: {},
-      style: {
-        plot: {
-          width: 200,
-          height: 30
-        },
-        resultHeight: 200
-      }
+  Vue.component('question-view', {
+    template: '#question-template',
+    props: ['options', 'questions', 'party', 'city', 'candidates'],
+    data() {
+      return {
+        opinions: {}
+      };
     },
     methods: {
-      textColorMap(x) {
-        if (x === undefined || x === null) return null;
-        return 'color: ' + colorMap(x, this.options.minScore, this.options.maxScore, 0.7);
-      },
-      getHTMLArrow(score) {
-        if (score > 0) return '&#8679;'; // arrow up
-        else if (score < 0) return '&#8681;'; // arrow down
-        return '&#9898;'; // hollow circle
-      },
-      toggleSelectedParty(party) {
-        if (this.selected.party === party) {
-          this.selected.party = null;
-        } else {
-          this.selected.party = party;
-        }
-      },
+      textColorMap(x) { return _textColorMap(x, this.options); },
+      getHTMLArrow(score) { return _getHTMLArrow(score); },
       toggleOpinion(id, sign) {
         if (this.opinions[id] === sign)
           this.opinions[id] = null;
@@ -103,23 +87,6 @@ function createApp(vueElement) {
         if (this.opinions[id] === sign) {
           return 'selected-' + (sign > 0 ? 'positive' : 'negative');
         }
-      },
-      truncateResults(results, rowHeight, margin = 0) {
-        const maxResults = Math.max(3, Math.floor((this.style.resultHeight-margin) / rowHeight));
-
-        if (results.length <= maxResults) return results;
-        const nTop = Math.ceil((maxResults-1)/2);
-        const nBottom = Math.floor((maxResults-1)/2);
-        return results.slice(0, nTop)
-          .concat([{ellipsis: true}])
-          .concat(results.slice(-nBottom));
-      },
-      setAnswerData(answers) {
-        model = new Model(answers);
-        this.cities = [...model.cities];
-        this.options = {...model.options};
-        this.candidates = [...model.candidates];
-        this.allParties = [...model.parties];
       }
     },
     watch: {
@@ -128,24 +95,12 @@ function createApp(vueElement) {
         this.questions.forEach(question => {
           Vue.set(this.opinions, question.id, null);
         });
+      },
+      nonEmptyOpinions() {
+        this.$emit('opinions-changed', this.nonEmptyOpinions);
       }
     },
     computed: {
-      parties() {
-        return this.allParties.filter(party => {
-          return !this.selected.city ||
-            this.candidates.filter(p =>
-              p.city === this.selected.city &&
-              p.party === party).length > 0;
-        });
-      },
-
-      questionsById() {
-        const qById = [];
-        this.questions.forEach(q => { qById[q.id] = q.question; });
-        return qById;
-      },
-
       nonEmptyOpinions() {
         const opinions = {};
         for (const [key, value] of Object.entries(this.opinions)) {
@@ -158,22 +113,11 @@ function createApp(vueElement) {
         return Object.keys(this.nonEmptyOpinions).length > 0;
       },
 
-      sortedParties() {
-        if (!this.anyOpinions)
-          return this.parties.map(p => ({ name: p }));
-
-        const maxR = this.style.plot.height * 0.7;
-        return this.truncateResults(
-          model.sortedParties(this.parties, this.nonEmptyOpinions),
-          this.style.plot.height + 10
-        );
-      },
-
       sortedQuestions() {
-        if (!this.selected.party) {
-          if (this.selected.city) {
+        if (!this.party) {
+          if (this.city) {
             // filter out questions with no answers from the selected city
-            const cityPeople = this.candidates.filter(p => p.city === this.selected.city);
+            const cityPeople = this.candidates.filter(p => p.city === this.city);
             return this.questions
               .filter(q => model.scorePeople({ [q.id]: 1 }, cityPeople).bins.length > 0);
           } else {
@@ -181,7 +125,7 @@ function createApp(vueElement) {
           }
         }
 
-        const partyPeople = this.candidates.filter(p => p.party === this.selected.party);
+        const partyPeople = this.candidates.filter(p => p.party === this.party);
         const questions = this.questions.map(question => ({
           score: model.scorePeople({ [question.id]: 1 }, partyPeople).score,
           ...question
@@ -199,19 +143,88 @@ function createApp(vueElement) {
             return {
               matchScore,
               shortTitle: (op*100) + ' vs ' + roundedScore,
-              matchTitle: this.selected.party + ': ' +  roundedScore,
+              matchTitle: this.party + ': ' +  roundedScore,
               ...q
             };
           })
           .filter(x => x !== null) // drop questions with now answers in this group
           .concat(questions.filter(q => !this.nonEmptyOpinions[q.id]));
+      }
+    }
+  });
+
+  Vue.component('result-view', {
+    template: '#result-template',
+    props: ['nonEmptyOpinions', 'city', 'options', 'candidates', 'allParties', 'questions'],
+    data() {
+      return {
+        selectedParty: null,
+        style: {
+          plot: {
+            width: 200,
+            height: 30
+          },
+          resultHeight: window.innerHeight / 2 // TODO document.getElementById('result-box').clientHeight;
+        }
+      };
+    },
+    methods: {
+      textColorMap(x) { return _textColorMap(x, this.options); },
+      getHTMLArrow(score) { return _getHTMLArrow(score); },
+      toggleSelectedParty(party) {
+        if (this.party === party) {
+          this.selectedParty = null;
+        } else {
+          this.selectedParty = party;
+        }
+      },
+      truncateResults(results, rowHeight, margin = 0) {
+        const maxResults = Math.max(3, Math.floor((this.style.resultHeight-margin) / rowHeight));
+
+        if (results.length <= maxResults) return results;
+        const nTop = Math.ceil((maxResults-1)/2);
+        const nBottom = Math.floor((maxResults-1)/2);
+        return results.slice(0, nTop)
+          .concat([{ellipsis: true}])
+          .concat(results.slice(-nBottom));
+      }
+    },
+    computed: {
+      questionsById() {
+        const qById = [];
+        this.questions.forEach(q => { qById[q.id] = q.question; });
+        return qById;
+      },
+
+      anyOpinions() {
+        return Object.keys(this.nonEmptyOpinions).length > 0;
+      },
+
+      parties() {
+        return this.allParties.filter(party => {
+          return !this.city ||
+            this.candidates.filter(p =>
+              p.city === this.city &&
+              p.party === party).length > 0;
+        });
+      },
+
+      sortedParties() {
+        if (!this.anyOpinions)
+          return this.parties.map(p => ({ name: p }));
+
+        const maxR = this.style.plot.height * 0.7;
+        return this.truncateResults(
+          model.sortedParties(this.parties, this.nonEmptyOpinions),
+          this.style.plot.height + 10
+        );
       },
 
       sortedCandidates() {
-        if (!this.selected.party) return [];
+        if (!this.selectedParty) return [];
         let partyPeople = this.candidates
-          .filter(p => p.party === this.selected.party)
-          .filter(p => !this.selected.city || p.city === this.selected.city);
+          .filter(p => p.party === this.selectedParty)
+          .filter(p => !this.city || p.city === this.city);
 
         const rowH = 28, margin = 40;
         if (!this.anyOpinions) return this.truncateResults(partyPeople, rowH, margin);
@@ -230,10 +243,45 @@ function createApp(vueElement) {
     }
   });
 
+  const app = new Vue({
+    el: vueElement,
+    data: {
+      copyright: {},
+      questions: [],
+      options: {},
+      cities: [],
+      candidates: [],
+      allParties: [],
+      nonEmptyOpinions: {},
+      error: null,
+      selected: {
+        party: null,
+        city: null
+      },
+      opinions: {}
+    },
+    methods: {
+      setAnswerData(answers) {
+        model = new Model(answers);
+        this.cities = [...model.cities];
+        this.options = {...model.options};
+        this.candidates = [...model.candidates];
+        this.allParties = [...model.parties];
+      },
+
+      opinionsChanged(opinions) {
+        this.nonEmptyOpinions = opinions;
+      },
+
+      partyChanged(party) {
+        this.selected.party = party;
+      }
+    }
+  });
+
   return {
     start({questions, answers, meta} = {}) {
       document.title = meta.title;
-      app.style.resultHeight = document.getElementById('result-box').clientHeight;
       app.questions = Object.keys(questions).map(q => ({
         id: q,
         question: questions[q]
